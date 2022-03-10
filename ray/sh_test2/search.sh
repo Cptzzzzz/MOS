7afb03eec90ef3ba12993bb27ba5781ba0cfbed2
Cptz <925471815@qq.com>
2022-03-10 17:30:57 +0800

    third commit

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,7 @@
-#!bin/bash
+#!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+
+grep $2 $1 -n > temp
+awk -F: '{print $1}' temp > $3
+rm temp
