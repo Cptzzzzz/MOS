4a8d9f382d5d3e8c9f63f1226f0e40f81acbfc39
Cptz <925471815@qq.com>
2022-03-22 16:45:54 +0800

    Lab1-3

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -63,7 +63,7 @@ int readelf(u_char *binary, int size)
         }
         shdr=(Elf32_Shdr*)(binary+ehdr->e_shoff);
         for(sh_entry_count=0;sh_entry_count<(ehdr->e_shnum);sh_entry_count++){
-                printf("%d:0x%x\n",sh_entry_count,shdr[sh_entry_count].sh_offset);
+                printf("%d:0x%x\n",sh_entry_count,shdr[sh_entry_count].sh_addr);
         }
         // get section table addr, section header number and section header size.
 
