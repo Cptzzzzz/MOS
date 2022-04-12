629e95317801eb81793f1f30e1e0ea28ffbd48df
Cptz <925471815@qq.com>
2022-04-12 20:48:33 +0800

    ac

--- include/queue.h
+++ include/queue.h
@@ -111,8 +111,8 @@
  */
 #define LIST_INSERT_AFTER(listelm, elm, field) do{                                              \
                 LIST_NEXT((elm),field)=LIST_NEXT((listelm),field);                              \
-                if(LIST_NEXT((elm),field)!=NULL)                                                \
-                        (LIST_NEXT((elm),field))->field.le_prev=&LIST_NEXT((elm),field);        \
+                if(LIST_NEXT((listelm),field)!=NULL)                                                \
+                        (LIST_NEXT((listelm),field))->field.le_prev=&LIST_NEXT((elm),field);        \
                 LIST_NEXT((listelm),field)=(elm);                                               \
                 (elm)->field.le_prev=&LIST_NEXT((listelm),field);                               \
         } while(0)
@@ -152,14 +152,14 @@
  * The "field" name is the link element as above. You can refer to LIST_INSERT_HEAD.
  * Note: this function has big differences with LIST_INSERT_HEAD !
  */
-#define LIST_INSERT_TAIL(head, elm, field)do{                                           \
-        if(LIST_FIRST((head))==NULL) LIST_INSERT_HEAD(head,elm,field);                  \
-        else{                                                                           \
-                (elm)->field.le_prev=&LIST_NEXT((LIST_FIRST(head)),field);              \
-                while( *(elm)->field.le_prev!=NULL)                                     \
-                        (elm)->field.le_prev=&LIST_NEXT( ( *(elm)->field.le_prev ),field );      \
-                LIST_NEXT((*(elm)->field.le_prev),field)=(elm);                         \
-        }                                                                               \
+#define LIST_INSERT_TAIL(head, elm, field)  do{                                           \
+                if(LIST_FIRST((head))==NULL) LIST_INSERT_HEAD(head,elm,field);                  \
+                else{                                                                           \
+                        (elm)->field.le_prev=&LIST_NEXT((LIST_FIRST(head)),field);              \
+                        while( *(elm)->field.le_prev!=NULL)                                     \
+                                (elm)->field.le_prev=&LIST_NEXT( ( *(elm)->field.le_prev ),field );      \
+                        *(elm)->field.le_prev=(elm);                         \
+                }                                                                               \
         }while(0)
 /* finish your code here. */
 
