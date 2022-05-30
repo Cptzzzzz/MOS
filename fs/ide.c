01f37b39ab8d59d52061afa0424c66b78d4be3e1
root <root@DESKTOP-QAENTOG.localdomain>
2022-05-30 12:47:14 +0800

    save

--- fs/ide.c
+++ fs/ide.c
@@ -29,28 +29,39 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 	int offset_begin = secno * 0x200;
 	int offset_end = offset_begin + nsecs * 0x200;
 	int offset = 0;
-
-	u_int zero=0;
-	u_int cur_offset=0;
+    u_int dev = 0x13000000;
+    u_char status = 0;
+    u_char read_mod = 0;
 
 	while (offset_begin + offset < offset_end) {
-		// Your code here
-		// error occurred, then panic.
-		cur_offset=offset_begin+offset;
-		if(syscall_write_dev((u_int)&diskno,0x13000010,4)<0)
-			user_panic("ids_read panic");
-		if(syscall_write_dev((u_int)&cur_offset,0x13000000,4)<0)
-			user_panic("ids_read panic");
-		if(syscall_write_dev((u_int)&zero,0x13000020,1)<0)
-			user_panic("ids_read panic");
-		u_int succ;
-		if(syscall_read_dev((u_int)&succ,0x13000030,1)<0)
-			user_panic("ide_read panic");
-		if(!succ)
-			user_panic("ide_read panic");
-		if(syscall_read_dev((u_int)(dst+offset),0x13004000,0x200)<0)
-			user_panic("ids_read panic");
-		offset+=0x200;
+        // Your code here
+        // error occurred, then panic.
+
+        u_int cur_offset = offset_begin + offset;
+        if (syscall_write_dev((u_int)&diskno, dev+0x10, 4)<0) {
+            user_panic("IDE_read_error when select by id!\n");
+        }
+
+        if (syscall_write_dev((u_int)&cur_offset, dev, 4)<0) {
+            user_panic("IDE_read_error when setting offset!\n");
+        }
+
+        if (syscall_write_dev((u_int)&read_mod, dev+0x20, 1)<0) {
+            user_panic("IDE_read_error when setting read_mod!\n");
+        }
+        status = 0;
+        if (syscall_read_dev((u_int)&status, dev+0x30, 1)<0) {
+            user_panic("IDE_read_error when getting IDE status!\n");
+        }
+
+        if (status==0) {
+            user_panic("IDE_read_error for IDE failed!\n");
+        }
+        if (syscall_read_dev((u_int)(dst+offset), dev+0x4000, 0x200)<0) {
+            user_panic("IDE_read_error when reading data!\n");
+        }
+
+        offset += 0x200;
 	}
 }
 
@@ -74,32 +85,40 @@ ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
 	// Your code here
 	int offset_begin = secno*0x200;
-	int offset_end = offset_begin+nsecs*0x200;
+	int offset_end = offset_begin + nsecs*0x200;
 	int offset = 0;
+    u_int dev = 0x13000000;
+    u_char status = 0;
+    u_char write_mod = 1;
 
-	u_int one=1;
-	u_int cur_offset=0;
 	// DO NOT DELETE WRITEF !!!
 	writef("diskno: %d\n", diskno);
 
-	while ( offset_begin+offset<offset_end ) {
-		// copy data from source array to disk buffer.
+	while (offset_begin + offset < offset_end) {
+	    // copy data from source array to disk buffer.
+        //if error occur, then panic.
 
-		// if error occur, then panic.
-		cur_offset=offset_begin+offset;
-		if (syscall_write_dev((u_int)(src + offset), 0x13004000, 0x200) < 0)
-			user_panic("ide_write panic");
-		if (syscall_write_dev((u_int)&diskno, 0x13000010, 4) < 0)
-			user_panic("ide_write panic");
-		if (syscall_write_dev((u_int)&cur_offset, 0x13000000, 4) < 0)
-			user_panic("ide_write panic");
-		if (syscall_write_dev((u_int)&one, 0x13000020, 1) < 0)
-			user_panic("ide_write panic");
-		u_int succ;
-		if (syscall_read_dev((u_int)&succ, 0x13000030, 1) < 0)
-			user_panic("ide_write panic");
-		if(!succ)
-			user_panic("ids_write panic");
-		offset += 0x200;
-	}
+        u_int cur_offset = offset_begin + offset;
+        if (syscall_write_dev((u_int)&diskno, dev+0x10, 4)<0) {
+            user_panic("IDE_write_error when select by id!\n");
+        }
+        if (syscall_write_dev((u_int)&cur_offset, dev, 4)<0) {
+            user_panic("IDE_write_error when setting offset!\n");
+        }
+        if (syscall_write_dev((u_int)(src+offset), dev+0x4000, 0x200)<0) {
+            user_panic("IDE_write_error when writing data!\n");
+        }
+        if (syscall_write_dev((u_int)&write_mod, dev+0x20, 1)<0) {
+            user_panic("IDE_write_error when setting write_mod!\n");
+        }
+        status = 0;
+        if (syscall_read_dev((u_int)&status, dev+0x30, 1)<0) {
+            user_panic("IDE_write_error when getting IDE status!\n");
+        }
+        if (status==0) {
+            user_panic("IDE_write_error for IDE failed!\n");
+        }
+
+        offset += 0x200;
+    }
 }
