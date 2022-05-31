fa243c8aa5b8013f6e3ac35ccedb2a06d939fedb
root <root@DESKTOP-QAENTOG.localdomain>
2022-05-31 17:55:18 +0800

    save

--- init/init.c
+++ init/init.c
@@ -15,9 +15,9 @@ void mips_init() {
 	env_init();
 
 	// ENV_CREATE(user_devtst);
-	ENV_CREATE(user_fstest);
+	// ENV_CREATE(user_fstest);
 	ENV_CREATE(fs_serv);
- 
+
 	trap_init();
 	kclock_init();
 
