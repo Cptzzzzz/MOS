a948d45563c48b4badce22a9956e69009b633732
root <root@DESKTOP-QAENTOG.localdomain>
2022-06-09 11:59:04 +0800

    test

--- fs/fs.c
+++ fs/fs.c
@@ -208,7 +208,7 @@ void
 free_block(u_int blockno)
 {
 	// Step 1: Check if the parameter `blockno` is valid (`blockno` can't be zero).
-	if(blockno==0||(super!=NULL&&blockno>=super->s_nblocks))return;
+	if(blockno==0||(super==NULL||blockno>=super->s_nblocks))return;
 
 	bitmap[blockno/32]|=(1<<(blockno%32));
 	// Step 2: Update the flag bit in bitmap.
