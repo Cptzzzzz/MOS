3e19af24f368f72698cd2399140f19ab75503489
root <root@DESKTOP-QAENTOG.localdomain>
2022-06-09 15:44:45 +0800

    save

--- fs/serv.c
+++ fs/serv.c
@@ -120,8 +120,11 @@ serve_open(u_int envid, struct Fsreq_open *rq)
 	//	user_panic("file_open failed: %d, invalid path: %s", r, path);
 		if(rq->req_omode&O_CREAT)
 			file_create(path,&f);
-		ipc_send(envid, r, 0, 0);
+		else{
+			ipc_send(envid, r, 0, 0);
 		return ;
+		}
+		
 	}
 
 	// Save the file pointer.
